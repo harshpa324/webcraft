b8ff92c0ef3585b2961430bf7fd7da98ec640284
harshpa324 <harshpa324@gmail.com>
2024-01-31 22:32:51 +0530

    final changes done for now

--- src/components/DraggableToolbar.jsx
+++ src/components/DraggableToolbar.jsx
@@ -12,10 +12,10 @@ import DraggableImage from './DraggableImage';
 const DraggableToolbar = () => {
   return (
     <div className='flex justify-center items-center md:items-start gap-5 pt-5 bg-gradient-to-b from-gray-200 via-gray-300 to-gray-500 h-[15vh] md:h-screen w-full md:w-[30vw]'>
-      <div className='bg-gray-400 hover hover:bg-purple-200 py-2 px-8 rounded-md cursor-move'>
+      <div className='bg-gray-400 hover hover:bg-gray-300 py-2 px-8 rounded-md cursor-move'>
         <DraggableText />
       </div>
-      <div className='bg-gray-400 hover hover:bg-purple-200 py-2 px-8 rounded-md cursor-move'>
+      <div className='bg-gray-400 hover hover:bg-gray-300 py-2 px-8 rounded-md cursor-move'>
         <DraggableImage />
       </div>
     </div>
